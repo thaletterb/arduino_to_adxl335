050f05fe155211857e8a1834bb1e13d57977991d
Brian <thaletterb@gmail.com>
2014-09-09 12:24:30 -0700

    add setup and loop functions

--- arduino_to_adxl335.ino
+++ arduino_to_adxl335.ino
@@ -5,4 +5,17 @@ Library to interface an Arduino Board (Nano) with an ADXL335 breakout board
 
 */
 
+// Analog Read Pins
+const int xPin = 0;
+const int yPin = 1;
+const int zPin = 2;
 
+void setup(){
+  // Initialize serial communication at 9600 bps Baud 
+  Serial.begin(9600);
+  
+}
+
+void loop(){
+  
+}
